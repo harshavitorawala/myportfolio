f4f01796b71faa2499e34a92ffab6b0b8183c02f
harshavitorawala <harshavitorawala@gmail.com>
2024-08-28 19:50:23 +0530

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,141 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg",
+    title: "Youtube - Like predictor",
+    description:
+      "Aim to predict the number of likes for upcoming videos on a YouTube channel using machine learning. It nvolves scraping data from the channel's previous videos and analyzing it to identify patterns and trends.",
+    tech_stack: ["Python", "Flask", "HTML", "CSS", "JavaScript"],
+    github_url: "https://github.com/harshavitorawala/YouTube-Like-Predictor",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKJXWCEwU5Gs-fFFjb084Swvnj4_ZLGodmMA&s",
+    title: "Red Cross Connect ",
+    description:
+      "An Online platform designed to connect donors with those in need of blood.",
+    tech_stack: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+    github_url: "https://github.com/harshavitorawala/Red-Cross-connect-BloodDonation",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQurBBCgOwtHQc3BOmqckBwt6yfHr60gl0iGQ&s",
+    title: "BelleBliss Cosmetix ",
+    description:
+      "An e-commerce website for beauty cosmetics.",
+    tech_stack: ["NodeJs", "MongoDB", "ReactJs"],
+    github_url: "https://github.com/harshavitorawala/beautycosmetic-ecommerce",
+  },
+  {
+    img: "https://www.edriving.com/three60/wp-content/uploads/sites/2/2021/10/helmet-scan-iStock-1316772747.jpg",
+    title: "Traffic Rule Violation: Helmet Detection",
+    description:
+      "Detection of traffic violations, specifically targeting non-helmet riders",
+    tech_stack: ["Python", "YOLOv8"],
+    github_url: "https://github.com/harshavitorawala/Helmet-detection-with-YOLO",
+  },
+  {
+    img: "https://lh4.googleusercontent.com/proxy/Hpsz3rYLCm9W6pySX3DcCbDdkZib3pP6IoRoVAjGc39dxkayWJOUbyLMnhbiY3T2f4JNX0D7r-nW20tcHzKjZ_9hVhc5JqI",
+    title: "Blood Bank",
+    description:
+      "A blood bank platform involves donor registration, availability updates, user blood request submissions, and donor request management.",
+    tech_stack: ["Laravel", "MySQL", "JavaScript"],
+    github_url: "https://github.com/harshavitorawala/blood-bank-laravel",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Y3AV_1e7QtqkkRKS69D9xDyhkmlJu-1uPw&s",
+    title: "ChatPulse - A Chat App ",
+    description:
+      "The application allows users to register, log in, and engage in private real-time messaging.",
+    tech_stack: ["Node.Js", "React", "MongoDB","SocketIO"],
+    github_url: "https://github.com/harshavitorawala/ChatApp",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/php/php-original.svg",
+    name: "PHP",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/laravel/laravel-original.svg",
+    name: "Laravel",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/django/django-plain.svg",
+    name: "Django",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+    name: "AWS",
+  },
+  
+];
